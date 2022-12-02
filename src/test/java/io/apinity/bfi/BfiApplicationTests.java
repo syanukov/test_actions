df95d13823322b18897aa7b9289cc19efbb684c8
Stanislau Yanukou <stanislau.yanukou@apinity.io>
2022-12-02 20:37:13 +0100

    skeleton

---
+++ src/test/java/io/apinity/bfi/BfiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.apinity.bfi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
