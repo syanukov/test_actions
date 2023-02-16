ef9e9a8ae2d63b0c4e6246a1b2186dee9c412df2
Stanislau Yanukou <stanislau.yanukou@apinity.io>
2023-02-16 11:29:11 +0100

    fix: dummy change in sout

--- src/main/java/io/apinity/bfi/BfiApplication.java
+++ src/main/java/io/apinity/bfi/BfiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BfiApplication {
 
   public static void main(String[] args) {
-    System.out.println("**** hello from skipped ***");
+    System.out.println("**** hello ***");
     SpringApplication.run(BfiApplication.class, args);
   }
 
